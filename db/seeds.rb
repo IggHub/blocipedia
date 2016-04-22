5dc96d487abc7ce2816abd949ed7d9a2f0bcdea3
Iggy <igoririanto@rocketmail.com>
2016-04-22 14:17:48 -0700

    Added faker gem and seeded db with users and basic wikis

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+10.times do
+    User.create!(
+      username: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: Faker::Internet.password
+    )
+end
+users = User.all
+
+50.times do
+  Wiki.create!(
+    title: Faker::Lorem.word,
+    body: Faker::Lorem.sentence,
+    user: users.sample
+  )
+end
+
+wiki = Wiki.all
+
+puts "Seeding done!"
+puts "#{User.count} users now exist"
+puts "#{Wiki.count} wikis now exist"
